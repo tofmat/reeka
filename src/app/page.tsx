79be7b8681277ae1baf939690800ad982513ee1a
Tosin Ogunfowote <tofmatt@gmail.com>
2024-04-23 09:24:33 +0100

    feat: pushed design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,12 @@
 import Footer from "@/components/footer";
 import { HeroScroll } from "@/components/heroScroll";
 import ProductOld from "@/components/product-old";
-import Products from "@/components/products";
-import ProductsMobile from "@/components/productsMobile";
 import PropertyManagement from "@/components/property-management";
 export default function Home() {
   return (
     <main>
       <HeroScroll />
       <PropertyManagement />
-      {/* <Products />
-      <ProductsMobile /> */}
       <ProductOld />
       <Footer />
     </main>
